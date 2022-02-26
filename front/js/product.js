5beaef620e2c521a55d0d37064566e255c5abf31
sly545 <sylvainferrandes@gmail.com>
2022-02-26 21:23:14 +0100

    ajout des ecouteur 'element et les diferrent kanap

--- front/js/product.js
+++ front/js/product.js
@@ -1,143 +1,118 @@
 let params = new URL (document.location).searchParams;
 let ID = params.get("ID");
-console.log(ID);
-const fetchkanap = () => {
+//console.log(ID);
+
+function fetchkanap() {
+  fetch(`http://localhost:3000/api/products/${ID}`) 
+    .then(convertJSON)
+    .then(function (dataFromAPI) {
+      //console.log(dataFromAPI); 
+      //const API
+      const namekanap = dataFromAPI.name;
+      const descriptionduKANAP = dataFromAPI.description;
+      const descriptionALT = dataFromAPI.altTxt;
+      const photo = dataFromAPI.imageUrl;
+      const balisephoto = document.getElementById("ok");
+      const selectioncolor = document.getElementById ("colors");
+      const prix = dataFromAPI.price;
+      //sellecteure d'element
+      const ajoutpanier = document.getElementById("addToCart");
+      let Idform =document.getElementById("quantity");
+      const choiceColors =document.getElementById("colors");
+      //choix des quantier 
+      
+      //selection du bouton pour que cette valeur se dimette dans la bonnne variable
+      
+      
+      
+
+      let images = document.createElement("img");
+        //atributs creation
+      images.setAttribute("src",photo);
+      images.setAttribute("alt",descriptionALT);
+      
+      let colorsK  =dataFromAPI.colors;
+      let tabscolors = [];
+      tabscolors = colorsK;
+      //boucle pour les couleurs
+      tabscolors.forEach(items => {
+
+      const evrycolors = items;
+      //console.log(evrycolors);
+      //console.log(selectioncolor);
+      let option =document.createElement("option");
+      document.getElementById("title");
+      document.getElementById("description");
+      document.getElementById("price");
+      title.textContent = namekanap;
+      description.textContent = descriptionduKANAP;
+      option.textContent= evrycolors;
+      price.textContent = prix;
+      balisephoto.appendChild(images);
+      selectioncolor.appendChild(option);
+      
+      
+    });
+    //Fonction qui va écouter le clic pour récupérer les différences informations sur la quantité 
+    //le nom du produit et la couleur.
     
-    fetch(`http://localhost:3000/api/products/${ID}`) 
-      .then(function (responseJSON) {
-        
-        if (responseJSON.ok) {
-          
-          return responseJSON.json();
-        }
-      })
-      .then(function (dataFromAPI) {
-        console.log(dataFromAPI); 
-        //const
-        const namekanap = dataFromAPI.name;
-        const prix = dataFromAPI.price;
-        const descriptionduKANAP = dataFromAPI.description; 
-        const descriptionALT = dataFromAPI.altTxt;
-        const photo = dataFromAPI.imageUrl;
-        let colors =dataFromAPI.colors;
-        console.table(colors); 
-        const elemenone = dataFromAPI.colors[0];
-        const elementwo = dataFromAPI.colors[1];
-        const elementree = dataFromAPI.colors[2];
-        const elementfor = dataFromAPI.colors[3];
-        
-        
-        
+    
+   
 
+    
+    
 
-        console.log(elementfor);
-        
-        //console.log(elemenone,elementwo,elementree,elementfor);
-        const balisephoto = document.getElementById("ok");
-        const selectioncolor = document.getElementById ("colors");
+    Idform.addEventListener("click", () => {
+      ajoutpanier.addEventListener('click',handleAddToCart);
+    
+    
+    
+      optionProduit = [namekanap,ID,choiceColors.value,Idform.value,prix]
       
-        
-        
-        
-        
-        
-        
-        
-        //console.log(namekanap,prix,description,descriptionALT,photo);
+     
+    
+   
+    
+    
+    function handleAddToCart() {
+      //console.log('Click sur ajout au panier');
+      console.log(optionProduit);
+      
+     
+    
+      // Recup les valeurs qu'il faut
+      // Les mettre dans un tableau 
+      // Et "sauvegarder" le tableau dans le local storage
+    }
+    
 
-        //creation des elements du dom
-        let images = document.createElement("img");
-        let option1 =document.createElement("option");
-        let option2 =document.createElement("option");
-        let option3 = document.createElement("option");
-        let option4 = document.createElement("option");
 
+    
 
-            
-        
-          
-       
-        
-
+  });
   
-        
-
-        
-        
-        
-        
-        //console.log(images,option1,option2,option3,option4);
-        
-       
-        option1.setAttribute("value",elemenone);
-        option2.setAttribute("value",elementwo);
-        option3.setAttribute("value",elementree)
-        option4.setAttribute("value",elementfor)
-        
-        
-        
-        images.setAttribute("src",photo);
-        images.setAttribute("alt",descriptionALT);
-        
-        document.getElementById("title");
-        document.getElementById("description");
-        document.getElementById("price");
-        
-        title.textContent = namekanap;
-        description.textContent = descriptionduKANAP;
-        option1.textContent= elemenone;
-        option2.textContent = elementwo;
-        option3.textContent = elementree;
-        option4.textContent = elementfor;
-        price.textContent = prix;
-        balisephoto.appendChild(images);
-        selectioncolor.appendChild(option1);
-        selectioncolor.appendChild(option2);
-        selectioncolor.appendChild(option3);
-        selectioncolor.appendChild(option4);
-
-        
-        
-
-
-        
-       
-        
-       
       
 
-        
-        
-
+  });
       
-        
+};
+ 
 
-        
-        
-        
-
-       
-        
-      
+//Fonction qui va écouter le clic pour récupérer les différences informations sur la quantité 
+//le nom du produit et la couleur.
 
 
 
-
-
-      
-        
-
-
-
-
-
-
-
-        
-        
-      });
-      
-  };
+function convertJSON(responseJSON) {
+  // verif si la reponse est ok
+  if (responseJSON.ok) {
+    // Dans ce cas : 
+    // - On formate la réponse
+    // - Puis on la retourne
+    return responseJSON.json();
+  }
+}
   
-  fetchkanap();
-  
+
+// Appel à la fonction
+fetchkanap();
--- front/js/script.js
+++ front/js/script.js
@@ -1,69 +1,87 @@
-const fetchkanap = () => {
+/**
+ * Fonction d'appel à l'API
+ * Recup tous les produits (canapés) 
+ */
+ function fetchKanap() {
   // On effectue un call à une API
   // => On envoie une requete à une API pour lui demander des informations
   fetch("http://localhost:3000/api/products") // On déclenche la requete sur l'URL suivante (= endpoint => là où notre API nous délivre des données)
-    .then(function (responseJSON) {
-      // Ici, une fois que la requete est effectuée
-      // On reçoit dans ce bloc then, là réponse de l'API au format JSON
-
-      // Si la réponse de l'API est ok
-      if (responseJSON.ok) {
-        // On retourne la réponse de l'API (qui est au format JSON)
-        // dans un format que JS va pouvoir utiliser pour travailler (un tableau d'objet JS)
-        return responseJSON.json();
-      }
-    })
-    .then(function (dataFromAPI) {
-      console.log(dataFromAPI);
-      //boucle sur l'api  
-      dataFromAPI.forEach(function (item) {
-        //const
-        const name = item.name;
-        const identifient = item._id;
-        const desriptifkanap = item.altTxt;
-        const descrpit = item.description;
-        const visuel = item.imageUrl;
-        const items = document.getElementById("items");
-
-        //variable
-
-        //console.log(name,identifient,desriptifkanap,descrpit,visuel);
-        //creation des elements du dom
-        let newlien = document.createElement("a");
-        let neawarticle = document.createElement("article");
-        let namekanap = document.createElement("h3");
-        let images = document.createElement("img");
-        let paragraphe = document.createElement("p");
-        //let lienpageproduit = "./product.html?";
-        let joined = `./product.html?ID=${identifient}`;
-        
-
-        
-        
-        
-        
-
-        //console.log(newlien,neawarticle,images,paragraphe,namekanap);
-        //ajout des class
-        namekanap.classList.add("productName");
-        paragraphe.classList.add("productDescription");
-        //ajout du texte
-        paragraphe.textContent = descrpit;
-        namekanap.textContent = name;
-        images.textContent = visuel;
-        //ajout des atributs
-        newlien.setAttribute("href",joined);
-        newlien.setAttribute("id", identifient);
-        images.setAttribute("src", visuel);
-        images.setAttribute("alt", desriptifkanap);
-        //indentation de la structur html
-        items.appendChild(newlien);
-        newlien.appendChild(neawarticle);
-        neawarticle.appendChild(images);
-        neawarticle.appendChild(namekanap);
-        neawarticle.appendChild(paragraphe);
-      });
-    });
+    .then(convertJSON)
+    .then(insertProductsOnDOM);
 };
 
-fetchkanap();
+/**
+ *    
+ * @param { Json } responseJSON 
+ * @returns { Array }
+ */
+function convertJSON(responseJSON) {
+  // Ici, une fois que la requete est effectuée
+  // On reçoit dans ce bloc then, là réponse de l'API au format JSON
+
+  // Si la réponse de l'API est ok
+  if (responseJSON.ok) {
+    // On retourne la réponse de l'API (qui est au format JSON)
+    // dans un format que JS va pouvoir utiliser pour travailler (un tableau d'objet JS)
+    return responseJSON.json();
+  }
+};
+
+/**
+ * Description simple et claire du but de la fontion
+ * @param { Array } dataFromAPI 
+ */
+function insertProductsOnDOM(dataFromAPI) {
+  // console.log(dataFromAPI);
+  //boucle sur l'api  
+  dataFromAPI.forEach(createProduct);
+}
+
+
+function createProduct(item) {
+  // Recup des données dans des variables
+  const name = item.name;
+  const identifient = item._id;
+  const desriptifkanap = item.altTxt;
+  const descrpit = item.description;
+  const visuel = item.imageUrl;
+
+  // Recup l'element HTML où inserer la liste des produits dans le DOM
+  const items = document.getElementById("items");
+
+  //console.log(name,identifient,desriptifkanap,descrpit,visuel);
+  //creation des elements du dom
+  const newlien = document.createElement("a");
+  const neawarticle = document.createElement("article");
+  const namekanap = document.createElement("h3");
+  const images = document.createElement("img");
+  const paragraphe = document.createElement("p");
+  // Création du lien vers la page détail de produit avec l'id du canapé
+  const joined = `./product.html?ID=${identifient}`;
+
+  //console.log(newlien,neawarticle,images,paragraphe,namekanap);
+  //ajout des class
+  namekanap.classList.add("productName");
+  paragraphe.classList.add("productDescription");
+  //ajout du texte
+  paragraphe.textContent = descrpit;
+  namekanap.textContent = name;
+  images.textContent = visuel;
+  //ajout des atributs
+  newlien.setAttribute("href",joined);
+  newlien.setAttribute("id", identifient);
+  images.setAttribute("src", visuel);
+  images.setAttribute("alt", desriptifkanap);
+  //indentation de la structur html
+  neawarticle.appendChild(images);
+  neawarticle.appendChild(namekanap);
+  neawarticle.appendChild(paragraphe);
+  newlien.appendChild(neawarticle);
+
+  // Insertion dans le DOM
+  items.appendChild(newlien);
+}
+
+
+// Appel à la fonction
+fetchKanap();
